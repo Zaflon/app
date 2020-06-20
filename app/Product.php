d6f190953c383a38a8bb465f92a990fd486626b0
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-06-20 19:46:38 -0300

    added Relationship Product / Brand

--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+final class Product extends Model
 {
-    //
+    /**
+     * Brand.
+     * 
+     * @param void.
+     */
+    public function brand(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(\App\Brand::class);
+    }
 }

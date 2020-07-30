7bd07ad2b8397e90ef056483567909d7c4656a6c
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-07-30 20:45:04 -0300

    Created Coupons Report

--- app/Coupon.php
+++ app/Coupon.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Coupon extends Model
 {
     /** @var array */
+    protected $fillable = ['name', 'detail', 'price'];
+
+    /** @var array */
     protected const DATA = [
         [
             \App\Helpers\DOM::ALIAS => '#',
@@ -39,6 +42,45 @@ class Coupon extends Model
         [
             \App\Helpers\DOM::ALIAS => 'Delete',
             \App\Helpers\DOM::TYPE => \App\Helpers\DOM::__DELETE,
+        ]
+    ];
+
+    /** @var array */
+    public const REPORT = [
+        \App\Report\Report::FIELDS => [
+            'id' => [
+                \App\Report\Report::WIDTH => 20,
+                \App\Report\Report::ACTIVE => true
+            ],
+            'name' => [
+                \App\Report\Report::WIDTH => 40,
+                \App\Report\Report::ACTIVE => true
+            ],
+            'detail' => [
+                \App\Report\Report::WIDTH => 40,
+                \App\Report\Report::ACTIVE => true
+            ],
+            'price' => [
+                \App\Report\Report::WIDTH => 20,
+                \App\Report\Report::ACTIVE => true
+            ],
+            'deleted_at' => [
+                \App\Report\Report::WIDTH => 25,
+                \App\Report\Report::ACTIVE => false
+            ],
+            'created_at' => [
+                \App\Report\Report::WIDTH => 50,
+                \App\Report\Report::ACTIVE => true
+            ],
+            'updated_at' => [
+                \App\Report\Report::WIDTH => 30,
+                \App\Report\Report::ACTIVE => true
+            ],
+        ],
+        \App\Report\Report::INFO => [
+            \App\Report\Report::NAME => 'COUPONS REPORT',
+            \App\Report\Report::FILENAME => 'CouponsReport',
+            \App\Report\Report::HEIGHT => 4
         ]
     ];
 

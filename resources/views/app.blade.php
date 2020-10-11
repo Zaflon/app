4af9eb37e7a65924e49537f4b24e33bae51abf78
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-11 14:05:03 -0300

    app.blade.php

--- resources/views/app.blade.php
+++
@@ -1,9 +0,0 @@
-<p align="center">
-    <img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="800">
-</p>
-
-<h1 align="center">Aplicativo de Gerenciamento de Tarefas</h1>
-
-<div align="center">
-    <a href="{{ route('Color.index') }}">Color</a>
-</div>
